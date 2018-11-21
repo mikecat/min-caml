5aa9d889ecec25dfd7f0e7c406c6efed7e1b5854
MikeCAT <mikecat@users.noreply.github.com>
2018-11-21 22:53:34 +0900

    add Syntax.copy and Type.copy

--- type.ml
+++ type.ml
@@ -10,3 +10,15 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec copy = function
+  | Unit -> Unit
+  | Bool -> Bool
+  | Int -> Int
+  | Float -> Float
+  | Fun(xs, y) -> Fun(List.map copy xs, copy y)
+  | Tuple(xs) -> Tuple(List.map copy xs)
+  | Array(t) -> Array(copy t)
+  | List(t) -> List(copy t)
+  | Var({ contents = None }) -> Var(ref None)
+  | Var({ contents = Some(t) }) -> Var(ref (Some(copy t)))
