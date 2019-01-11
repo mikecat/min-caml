a1ffc4762d1805e800b67baaba0d5e59b1b389b1
MikeCAT <mikecat@users.noreply.github.com>
2019-01-12 08:41:32 +0900

    partial support of multiple function definition in closure.ml
    In this version, multiple definitions are emitted with the same name.
    so it will result in compile error and actually not supported.

--- closure.ml
+++ closure.ml
@@ -64,10 +64,11 @@ let rec g env known = function (* クロージャ変換ルーチン本体 (caml2html: closure
   | KNormal.IfLE(x, y, e1, e2) -> IfLE(x, y, g env known e1, g env known e2)
   | KNormal.Let((x, t), e1, e2) -> Let((x, t), g env known e1, g (M.add x t env) known e2)
   | KNormal.Var(x) -> Var(x)
-  | KNormal.LetRec(_, { KNormal.name = (x, t); KNormal.args = yts; KNormal.body = e1 }::_, e2) -> (* 関数定義の場合 (caml2html: closure_letrec) *)
+  | KNormal.LetRec((mx, mt), defs, e2) -> (* 関数定義の場合 (caml2html: closure_letrec) *)
       (* 関数定義let rec x y1 ... yn = e1 in e2の場合は、
          xに自由変数がない(closureを介さずdirectに呼び出せる)
-         と仮定し、knownに追加してe1をクロージャ変換してみる *)
+         と仮定し、knownに追加して最初の定義のe1をクロージャ変換してみる *)
+      let { KNormal.name = (x, t); KNormal.args = yts; KNormal.body = e1 } = List.hd defs in
       let toplevel_backup = !toplevel in
       let env' = M.add x t env in
       let known' = S.add x known in
@@ -84,12 +85,23 @@ let rec g env known = function (* クロージャ変換ルーチン本体 (caml2html: closure
          toplevel := toplevel_backup;
          let e1' = g (M.add_list yts env') known e1 in
          known, e1') in
+      (* この後本番の変換をするので、状態を戻す *)
+      toplevel := toplevel_backup;
+      (* 自由変数の名前は各定義で共通のはず *)
       let zs = S.elements (S.diff (fv e1') (S.add x (S.of_list (List.map fst yts)))) in (* 自由変数のリスト *)
-      let zts = List.map (fun z -> (z, M.find z env')) zs in (* ここで自由変数zの型を引くために引数envが必要 *)
-      toplevel := { name = (Id.L(x), t); args = yts; formal_fv = zts; body = e1' } :: !toplevel; (* トップレベル関数を追加 *)
+      (* envとenv'の差はxを入れたかどうかだけで、xは自由変数ではないので関係ない *)
+      let zts = List.map (fun z -> (z, M.find z env)) zs in (* ここで自由変数zの型を引くために引数envが必要 *)
+      (* ここでxをknownに追加するべきかがわかったので、その情報を利用して各定義の変換を行う *)
+      List.iter (fun { KNormal.name = (x, t); KNormal.args = yts; KNormal.body = e1 } ->
+        let env' = M.add x t env in
+        let e1' = g (M.add_list yts env') known' e1 in
+        toplevel := { name = (Id.L(x), t); args = yts; formal_fv = zts; body = e1' } :: !toplevel (* トップレベル関数を追加 *)
+      ) defs;
+      (* 各定義をマージした情報を用いて、その後の処理をさせる *)
+      let env' = M.add mx mt env in
       let e2' = g env' known' e2 in
-      if S.mem x (fv e2') then (* xが変数としてe2'に出現するか *)
-        MakeCls((x, t), { entry = Id.L(x); actual_fv = zs }, e2') (* 出現していたら削除しない *)
+      if S.mem mx (fv e2') then (* mxが変数としてe2'に出現するか *)
+        MakeCls((mx, mt), { entry = Id.L(mx); actual_fv = zs }, e2') (* 出現していたら削除しない *)
       else
         (Format.eprintf "eliminating closure(s) %s@." x;
          e2') (* 出現しなければMakeClsを削除 *)
